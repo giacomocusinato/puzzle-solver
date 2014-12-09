04f8f075c80624dd8f432e495f95fca6875ee2a7
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2014-12-09 21:24:24 +0100

    Added part-1 (initial version)

---
+++ part-1/src/Block.java
@@ -0,0 +1,89 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Block
+ *
+ * Represent a single puzzle block.
+ *
+ * Part of the PuzzleResolver project for Parallel and Concurrent Programming
+ * teaching at Padova university (Università degli Studi di Padova).
+ *
+ * @author Giacomo Cusinato
+ * @version 1.0
+ */
+
+public class Block {
+    private String id;
+    private String value;
+    private String topId;
+    private String rightId;
+    private String bottomId;
+    private String leftId;
+
+    public static String EMPTY_BLOCK = "VUOTO";
+
+    public Block(String uniqueId, String val, String top, String right,String bottom, String left) {
+        id = uniqueId;
+        value = val;
+        topId = top;
+        rightId = right;
+        bottomId = bottom;
+        leftId = left;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getTopId() {
+        return topId;
+    }
+
+    public String getBottomId() {
+        return bottomId;
+    }
+
+    public String getLeftId() {
+        return leftId;
+    }
+
+    public String getRightId() {
+        return rightId;
+    }
+
+    public void setId(String uniqueId) {
+        id = uniqueId;
+    }
+
+    public void setValue(String val) {
+        value = val;
+    }
+
+    public void setTopId(String top) {
+        topId = top;
+    }
+
+    public void setRightId(String right) {
+        rightId = right;
+    }
+
+    public void setBottomId(String bottom) {
+        bottomId = bottom;
+    }
+
+    public void setLeftId(String left) {
+        leftId = left;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\t%s\t%s\t%s\t%s\t%s",
+                id, value, topId, rightId, bottomId, leftId);
+    }
+}
---
+++ part-1/src/IOHelper.java
@@ -0,0 +1,83 @@
+import com.sun.javafx.geom.AreaOp;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Giacomo on 07/12/2014.
+ */
+public class IOHelper {
+    private static Charset charset = StandardCharsets.UTF_8;
+
+    /**
+     * Performs a read in the file with the given path
+     * @param inputFile
+     * @return
+     */
+    public static String readString(String inputFile) {
+        Path inputPath = null;
+        try {
+            inputPath = Paths.get(inputFile);
+        } catch (InvalidPathException ex) {
+            exceptionHelper("Invalid input path", ex.getMessage());
+            return  "";
+        }
+
+        StringBuilder content = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = Files.newBufferedReader(inputPath, charset);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                content.append(line + System.lineSeparator());
+            }
+        } catch (IOException ex) {
+            exceptionHelper("An IO error has occurred.", ex.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    exceptionHelper("An IO error has occurred.", ex.getMessage());
+                }
+            }
+        }
+
+        return content.toString();
+    }
+
+    /**
+     * Writes the given String in the file with the given path
+     * @param outputFile
+     * @param content
+     */
+    public  static void writeString(String outputFile, String content) {
+        BufferedWriter writer = null;
+        try {
+/*
+            File yourFile = new File(outputFile);
+            if(!yourFile.exists()) {
+                yourFile.createNewFile();
+            }
+*/
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(outputFile), "UTF-8"));
+            writer.write(content);
+            writer.close();
+        } catch (IOException ex) {
+            //exceptionHelper("An IO error has occurred.", ex.printStackTrace());
+            ex.printStackTrace();
+        }
+
+    }
+
+    private static void exceptionHelper(String message, String errorMessage) {
+        System.out.println(message);
+        if (errorMessage != null)
+            System.out.println(String.format("Error message: %s", errorMessage));
+    }}
